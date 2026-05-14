44eaf49ec9ffecc3bff700a58e05fce6fa12318a
JeffersonXd <196490824+JeffersonCMP1@users.noreply.github.com>
2026-05-13 22:02:32 -0500

    anadir imagen a app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import freecodecamplogo from './imagenes/freecodecamp-logo.png';
+import './imagenes/freecodecamp-logo.png';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='freecodecamp-logo-contenedor'>
+         {/* <img src={require(`./imagenes/freecodecamp-logo.png`)}  alt='Logo de freeCodeCamp'/> */}
+
+        <img src={freecodecamplogo} className='freecodecamp-logo' alt='Logo de freeCodeCamp'/>
+      </div>
     </div>
   );
 }
