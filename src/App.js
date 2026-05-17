501a2057a8d3782e7bac3d4be38b769e99c45565
JeffersonXd <196490824+JeffersonCMP1@users.noreply.github.com>
2026-05-17 12:03:59 -0500

    anadir estilos css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import freecodecamplogo from './imagenes/freecodecamp-logo.png';
 import './imagenes/freecodecamp-logo.png';
+import Boton from './componentes/Boton';
+
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,11 @@ function App() {
       </div>
 
       <div className='contenedor-calculadora'>
+        <Boton>1</Boton>
+        <Boton>+</Boton>
+        <Boton>3</Boton>
+
+        <Boton nombre='xd' gay='tu'></Boton>
         <div className='fila'></div>
         <div className='fila'></div>
         <div className='fila'></div>
--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,11 +1,19 @@
 import React from "react";
+import '../hojas-de-estilo/Boton.css';
 
 function Boton(props){
+
+    //
+    const esOperador=valor=>{
+        return isNaN(valor)&&(valor != '.')&&(valor != '=')
+    };
     return(
-        <div>
-            {props.children}
+        <div className={`boton-contenedor ${esOperador(props.children)?'operador':''}`.trimEnd()}>  
+            {props.children} 
         </div>
-    )
+    ) 
 }
 
 export default Boton;
+
+//explicacion el trimEnd, sirve para borrar espacios del final
