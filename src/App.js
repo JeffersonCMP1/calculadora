1d8a4b02bcad93d42878980776febea8a1f5df8d
JeffersonXd <196490824+JeffersonCMP1@users.noreply.github.com>
2026-05-18 01:38:10 -0500

    nuevo avance 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import freecodecamplogo from './imagenes/freecodecamp-logo.png';
 import './imagenes/freecodecamp-logo.png';
 import Boton from './componentes/Boton';
+import Pantalla from './componentes/Pantalla';
 
 function App() {
   return (
@@ -13,15 +14,31 @@ function App() {
       </div>
 
       <div className='contenedor-calculadora'>
-        <Boton>1</Boton>
-        <Boton>+</Boton>
-        <Boton>3</Boton>
-
-        <Boton nombre='xd' gay='tu'></Boton>
-        <div className='fila'></div>
-        <div className='fila'></div>
-        <div className='fila'></div>
-        <div className='fila'></div>
+        <Pantalla/>
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
         <div className='fila'></div>
       </div>
     </div>
